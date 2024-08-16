5d5a636b1c8c7e2db52809c34b1544fc4dad64ff
Cristian Bauza <cristian.bauza.silva@gmail.com>
2024-08-15 21:43:29 -0300

    First Commit

---
+++ DAL/DALs/DAL_Personas_ADONET.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DALs
+{
+    public class DAL_Personas_ADONET : IDAL_Personas
+    {
+        public void AddPersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePersona(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona GetPersona(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Persona> GetPersonas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePersona(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Practico1/Program.cs
@@ -0,0 +1,61 @@
+﻿using DAL.DALs;
+using DAL.IDALs;
+using Shared;
+
+IDAL_Personas dal = new DAL_Personas_Mock();
+
+string comando = "NUEVA";
+
+Console.WriteLine("Bienvenido a mi primera app .NET!!!");
+
+do
+{
+    Console.WriteLine("Ingrese comando (NUEVA/IMPRIMIR/EXIT): ");
+
+    try
+    {
+        comando = Console.ReadLine().ToUpper();
+
+        switch (comando)
+        {
+            case "NUEVA":
+
+                Persona persona = new Persona();
+                Console.WriteLine("Ingrese el nombre de la persona: ");
+                persona.Nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el documento de la persona: ");
+                persona.Documento = Console.ReadLine();
+                dal.AddPersona(persona);
+                break;
+
+            case "IMPRIMIR":
+
+                Console.WriteLine("Ingrese Nombre o Documento:");
+                string filtro = Console.ReadLine();
+
+                List<Persona> filtradas = 
+                    dal.GetPersonas().Where(p => p.Nombre.Contains(filtro) || p.Documento.Contains(filtro))
+                            .OrderBy(p => p.Nombre)
+                            .ToList();
+
+                foreach (Persona p in filtradas)
+                    Console.WriteLine(p.GetString());
+                break;
+
+            case "EXIT":
+                break;
+
+            default:
+                Console.WriteLine("Comando no reconocido.");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+while (comando != "EXIT");
+
+Console.WriteLine("Hasta luego!!!");
+Console.ReadLine();
